cf4297379da1d265dd4fa8dcbe96152c5f80da43
DylonTan <dylontan.2003@gmail.com>
2020-09-08 18:32:17 +0800

    Update src

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  useEffect(() => {
+    const connection = new WebSocket("ws://esp8266.local:80");
+
+    connection.onopen = () => {
+      console.log('Connected')
+    };
+
+    connection.onmessage = (res) => {
+      console.log(res.data);
+    };
+
+    connection.onerror = (err) => {
+      console.log("WebSocket Error: " + err);
+    };
+
+    connection.onclose = () => {
+      console.log("Disconnected");
+    };
+
+    window.addEventListener("keypress", (event) => {
+      console.log(connection.readyState, WebSocket.OPEN)
+      if (connection.readyState !== WebSocket.OPEN) {
+        return;
+      }
+
+      const keyCode = event.keyCode;
+
+      if (keyCode === 119) {
+        connection.send("FORWARDS");
+      } else if (keyCode === 97) {
+        connection.send("LEFT");
+      } else if (keyCode === 100) {
+        connection.send("RIGHT");
+      } else if (keyCode === 115) {
+        connection.send("BACKWARDS");
+      }
+    });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello why u wanna control me weirdo? </h1>
+      <p>Press W, A, S or D to control</p>
     </div>
   );
-}
+};
 
 export default App;
